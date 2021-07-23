975b778be6efab835ad69581cf46e5467fad20f3
chenjiekun <chenjiekun@sina.cn>
2021-07-24 05:32:22 +0800

    修改测试文件

--- auth/aksk/aksk_test.go
+++ auth/aksk/aksk_test.go
@@ -57,7 +57,7 @@ func createTestContext(){
 
 	//使用错误sk加密后的签名
 	request2, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
-	request1.Header.Set("Authorization-Type", "ak/sk")
+	request2.Header.Set("Authorization-Type", "ak/sk")
 	request2.Header.Set("Content-Type", "application/json")
 	request2.Header.Set("x-gateway-date", "20200605T104456Z")
 	request2.Header.Set("Authorization", "HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;host;x-gateway-date, Signature=bb18110ddf327a9c1222a551527896d59cb854ca9084078cfa3a6eb23de3ddb8")
@@ -66,10 +66,10 @@ func createTestContext(){
 
 	//传输了不存在的ak
 	request3, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
-	request1.Header.Set("Authorization-Type", "ak/sk")
+	request3.Header.Set("Authorization-Type", "ak/sk")
 	request3.Header.Set("Content-Type", "application/json")
 	request3.Header.Set("x-gateway-date", "20200605T104456Z")
-	request3.Header.Set("Authorization", "HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;host;x-gateway-date, Signature=0c3d2598d931f36ca7d261d52dcd29f09d6573671bd593b7cbc55f73eb942758")
+	request3.Header.Set("Authorization", "HMAC-SHA256 Access=dsaasdasda, SignedHeaders=content-type;host;x-gateway-date, Signature=0c3d2598d931f36ca7d261d52dcd29f09d6573671bd593b7cbc55f73eb942758")
 	Context3 := http_context.NewContext(request3, &writer{})
 	testContexts = append(testContexts, Context3)
 }
