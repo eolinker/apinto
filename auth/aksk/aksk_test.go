f80eb0c619a3c589bd666d65718943721ebbe67a
chenjiekun <chenjiekun@sina.cn>
2021-07-24 05:10:21 +0800

    提交测试文件

--- auth/aksk/aksk_test.go
+++ auth/aksk/aksk_test.go
@@ -3,7 +3,6 @@ package aksk
 import (
 	http_context "github.com/eolinker/goku-eosc/node/http-context"
 	"io"
-	"log"
 	"net/http"
 	"testing"
 )
@@ -11,25 +10,68 @@ import (
 var akskConfig = map[string]AKSKConfig{
 	"4c897cfdfca60a59983adc2627942e7e": {
 		AK:             "4c897cfdfca60a59983adc2627942e7e",
+		SK: 			"6bb8eee91f88336dd95b88a66709f0a3286ce1abf73453acc4619bc142d64040",
 		HideCredential: true,
 		Labels:         map[string]string{},
 		Expire:         1658740726, //2022-07-25 17:18:46
 	},
 }
 
+var testContexts = make([]*http_context.Context, 0, 10)
+
 func TestAKSK(t *testing.T) {
-	//aksk := &aksk{
-	//	id:         "123",
-	//	name:       "name",
-	//	akskConfig: akskConfig,
-	//}
-	request, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("x-gateway-date", "20200605T104456Z")
-	authorization := "Authorization: HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;x-gateway-date, Signature=123123"
-	oldContext := http_context.NewContext(request, &writer{})
-	parseAuthorization(oldContext)
-	log.Println(oldContext)
+	testAKSK := &aksk{
+		id:         "123",
+		name:       "name",
+		akskConfig: akskConfig,
+	}
+
+	createTestContext()
+
+	err := testAKSK.Auth(testContexts[0])
+	if err != nil {
+		t.Errorf("测试1：预期是能够通过鉴权，结果是%s", err.Error())
+	}
+
+	err = testAKSK.Auth(testContexts[1])
+	if err == nil {
+		t.Errorf("测试2：预期是不能够通过鉴权%s:，结果是nil", err.Error())
+	}
+
+	err = testAKSK.Auth(testContexts[2])
+	if err == nil {
+		t.Errorf("测试3：预期是不能够通过鉴权%s:，结果是nil", err.Error())
+	}
+}
+
+
+func createTestContext(){
+	//使用正确sk加密后的签名
+	request1, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
+	request1.Header.Set("Authorization-Type", "ak/sk")
+	request1.Header.Set("Content-Type", "application/json")
+	request1.Header.Set("x-gateway-date", "20200605T104456Z")
+	request1.Header.Set("Authorization", "HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;host;x-gateway-date, Signature=0c3d2598d931f36ca7d261d52dcd29f09d6573671bd593b7cbc55f73eb942758")
+	Context1 := http_context.NewContext(request1, &writer{})
+	testContexts = append(testContexts, Context1)
+
+	//使用错误sk加密后的签名
+	request2, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
+	request1.Header.Set("Authorization-Type", "ak/sk")
+	request2.Header.Set("Content-Type", "application/json")
+	request2.Header.Set("x-gateway-date", "20200605T104456Z")
+	request2.Header.Set("Authorization", "HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;host;x-gateway-date, Signature=bb18110ddf327a9c1222a551527896d59cb854ca9084078cfa3a6eb23de3ddb8")
+	Context2 := http_context.NewContext(request2, &writer{})
+	testContexts = append(testContexts, Context2)
+
+	//传输了不存在的ak
+	request3, _ := http.NewRequest("GET", "http://www.demo.com/demo/login?parm1=value1&parm2=", &body{})
+	request1.Header.Set("Authorization-Type", "ak/sk")
+	request3.Header.Set("Content-Type", "application/json")
+	request3.Header.Set("x-gateway-date", "20200605T104456Z")
+	request3.Header.Set("Authorization", "HMAC-SHA256 Access=4c897cfdfca60a59983adc2627942e7e, SignedHeaders=content-type;host;x-gateway-date, Signature=0c3d2598d931f36ca7d261d52dcd29f09d6573671bd593b7cbc55f73eb942758")
+	Context3 := http_context.NewContext(request3, &writer{})
+	testContexts = append(testContexts, Context3)
 }
 
 type body struct {
