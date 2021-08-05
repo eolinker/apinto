0560ee171f2c78bd720cc7f3b210ce3039670a6e
Liujian <824010343@qq.com>
2021-08-05 15:28:39 +0800

    pprof测试

--- app/test/main.go
+++ app/test/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	http_context "github.com/eolinker/goku-eosc/node/http-context"
+	"sort"
 )
 
 func main() {
@@ -12,12 +10,29 @@ func main() {
 	//i:=strings.Index(a,"=")
 	//fmt.Println(a[:i])
 	//fmt.Println(a[i+1:])
-	a := "*"
-	fmt.Println(a[1:])
-	err := http.ListenAndServeTLS(":8181", "eolinker.csr", "eolinker.key", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//a := "*"
+	//fmt.Println(a[1:])
+	//err := http.ListenAndServeTLS(":8181", "eolinker.csr", "eolinker.key", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//
+	//	ctx := http_context.NewContext(r, w)
+	//	ctx.ProxyRequest.Headers()
+	//}))
+	//fmt.Println(err)
+	demo := []int{4, 1, 5, 2, 5, 15}
+	sort.Sort(IntSlice(demo))
+	fmt.Println(demo)
+}
 
-		ctx := http_context.NewContext(r, w)
-		ctx.ProxyRequest.Headers()
-	}))
-	fmt.Println(err)
+type IntSlice []int
+
+func (a IntSlice) Len() int {
+	return len(a)
+}
+
+func (a IntSlice) Less(i, j int) bool {
+	return a[i] < a[j]
+}
+
+func (a IntSlice) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
 }
