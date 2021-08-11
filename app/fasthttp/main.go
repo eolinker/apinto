fc8bcca87a35a17f67e697b69226cc2722b1161d
Liujian <824010343@qq.com>
2021-08-11 16:39:10 +0800

    加延迟

--- app/fasthttp/main.go
+++ app/fasthttp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"time"
 
 	"github.com/valyala/fasthttp"
 )
@@ -13,7 +14,7 @@ func main() {
 	}
 	s := &fasthttp.Server{
 		Handler: func(ctx *fasthttp.RequestCtx) {
-			//time.Sleep(10 * time.Millisecond)
+			time.Sleep(10 * time.Millisecond)
 			ctx.SetStatusCode(200)
 			ctx.Write(data)
 		},
