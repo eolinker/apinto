69e8686e8f634ddd11e94b0f81ab5a3bad039021
Liujian <824010343@qq.com>
2021-08-11 14:52:40 +0800

    修改fasthttp测试程序

--- app/fasthttp/main.go
+++ app/fasthttp/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"unsafe"
+	"io/ioutil"
+	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 func main() {
-	data := make([]byte, 8*1024*1024)
-	for i := range data {
-		data[i] = 1
+	data, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
 	}
-	fmt.Printf("n2 的类型 %T n2占中的大小是 %d G", data, unsafe.Sizeof(data))
-	//s := &fasthttp.Server{
-	//	Handler: func(ctx *fasthttp.RequestCtx) {
-	//
-	//		ctx.SetStatusCode(200)
-	//		ctx.Write([]byte("ok"))
-	//	},
-	//}
-	//s.ListenAndServe(":8082")
+	s := &fasthttp.Server{
+		Handler: func(ctx *fasthttp.RequestCtx) {
+			time.Sleep(10 * time.Millisecond)
+			ctx.SetStatusCode(200)
+			ctx.Write(data)
+		},
+	}
+	s.ListenAndServe(":8082")
 }
