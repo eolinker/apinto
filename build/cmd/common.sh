0a9840091a7d2172b325bd956d5b232a17696749
Liujian <824010343@qq.com>
2021-08-14 10:37:57 +0800

    修改压缩包文件名称

--- build/cmd/common.sh
+++ build/cmd/common.sh
@@ -45,7 +45,7 @@ function packageApp(){
     APP=$1
     VERSION=$2
     cd "${BasePath}/out/"
-    tar -zcf "${BasePath}/out/${APP}-${VERSION}.tar.gz"  "${APP}-${VERSION}"
+    tar -zcf "${BasePath}/out/${APP}-${VERSION}.linux.x64.tar.gz"  "${APP}-${VERSION}"
     cd "${BasePath}"
 }
 
