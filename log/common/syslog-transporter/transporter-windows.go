235309e00eb44460d2c7ef8e9d80bd61e775023a
chenjiekun <chenjiekun@sina.cn>
2021-08-22 13:34:51 +0800

    修改syslog transporter

--- log/common/syslog-transporter/transporter-windows.go
+++ log/common/syslog-transporter/transporter-windows.go
@@ -1,6 +1,12 @@
 //+build windows
 package syslog_transporter
 
-//func CreateTransporter(conf *Config, formatter eosc_log.Formatter) (log.TransporterReset, error) {
-//	return nil, fmt.Errorf("can not create syslog transporterReset in windows system")
-//}
+import (
+	"fmt"
+	eosc_log "github.com/eolinker/eosc/log"
+	"github.com/eolinker/goku/log"
+)
+
+func CreateTransporter(conf *Config, formatter eosc_log.Formatter) (log.TransporterReset, error) {
+	return nil, fmt.Errorf("can not create syslog transporterReset in windows system")
+}
