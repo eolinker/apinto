970c96d2c9b42f627426ea3acdc9efbb50e918e3
Liujian <824010343@qq.com>
2021-09-10 10:24:36 +0800

    将memory-store移除

--- app/goku/inner.go
+++ app/goku/inner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	store_memory "github.com/eolinker/eosc/store-memory"
 	"github.com/eolinker/goku/drivers/auth/aksk"
 	"github.com/eolinker/goku/drivers/auth/apikey"
 	"github.com/eolinker/goku/drivers/auth/basic"
@@ -16,7 +17,6 @@ import (
 	http_router "github.com/eolinker/goku/drivers/router/http-router"
 	service_http "github.com/eolinker/goku/drivers/service/service-http"
 	upstream_http "github.com/eolinker/goku/drivers/upstream/upstream-http"
-	store_memory "github.com/eolinker/goku/store-memory"
 )
 
 //Register 注册各类驱动工厂
