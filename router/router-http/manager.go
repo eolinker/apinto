f506b21dfb15499a8a81006d735ba880e7e98ec4
黄孟柱 <hmzzrcs@163.com>
2021-09-27 18:20:31 +0800

    提交

--- router/router-http/manager.go
+++ router/router-http/manager.go
@@ -1,18 +1,13 @@
 package router_http
 
 import (
-	"crypto/tls"
 	"errors"
-	"net"
-	"strings"
 	"sync"
+
+	traffic_http_fast "github.com/eolinker/eosc/traffic/traffic-http-fast"
 
 	"github.com/eolinker/eosc/common/bean"
 	"github.com/eolinker/eosc/traffic"
-
-	"github.com/valyala/fasthttp"
-
-	"github.com/eolinker/eosc/listener"
 )
 
 var _ iManager = (*Manager)(nil)
@@ -27,73 +22,44 @@ type iManager interface {
 	Cancel()
 }
 
-var manager = NewManager()
+var manager iManager
+
+func init() {
+	var tf traffic.ITraffic
+	bean.Autowired(&tf)
+
+	bean.AddInitializingBeanFunc(func() {
+		manager = NewManager(tf)
+	})
+}
 
 //Manager 路由管理器结构体
 type Manager struct {
-	locker    sync.Mutex
-	routers   IRouters
-	servers   map[int]*httpServer
-	listeners map[int]net.Listener
+	locker  sync.Mutex
+	routers IRouters
 
-	traffic traffic.ITraffic
-}
-
-type httpServer struct {
-	tlsConfig *tls.Config
-	port      int
-	protocol  string
-	srv       *fasthttp.Server
-	certs     *Certs
-}
-
-//shutdown 关闭http服务器
-func (h *httpServer) shutdown() {
-	h.srv.Shutdown()
-}
-
-//GetCertificate 获取证书配置
-func (h *httpServer) GetCertificate(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	if h.certs == nil {
-		return nil, errorCertificateNotExit
-	}
-	certificate, has := h.certs.Get(strings.ToLower(info.ServerName))
-	if !has {
-		return nil, errorCertificateNotExit
-	}
-
-	return certificate, nil
+	tf traffic_http_fast.IHttpTraffic
 }
 
 //Cancel 关闭路由管理器
 func (m *Manager) Cancel() {
 	m.locker.Lock()
 	defer m.locker.Unlock()
-	for p, s := range m.servers {
-		s.shutdown()
-		delete(m.servers, p)
-	}
 
-	for k, l := range m.listeners {
-		l.Close()
-		delete(m.listeners, k)
-	}
+	m.tf.Close()
+	m.tf = nil
+
 }
 
 //NewManager 创建路由管理器
-func NewManager() *Manager {
-	var traffic traffic.ITraffic
-	bean.Autowired(&traffic)
-	bean.AddInitializingBeanFunc(func() {
+func NewManager(tf traffic.ITraffic) *Manager {
 
-	})
-	return &Manager{
-
-		routers:   NewRouters(),
-		servers:   make(map[int]*httpServer),
-		listeners: make(map[int]net.Listener),
-		locker:    sync.Mutex{},
+	m := &Manager{
+		routers: NewRouters(),
+		tf:      traffic_http_fast.NewHttpTraffic(tf),
+		locker:  sync.Mutex{},
 	}
+	return m
 }
 
 //Add 新增路由配置到路由管理器中
@@ -101,32 +67,39 @@ func (m *Manager) Add(port int, id string, config *Config) error {
 	m.locker.Lock()
 	defer m.locker.Unlock()
 
-	router, isCreate, err := m.routers.Set(port, id, config)
+	router, _, err := m.routers.Set(port, id, config)
 	if err != nil {
 		return err
 	}
-	if isCreate {
-		s, has := m.servers[port]
-		if !has {
-			s = &httpServer{srv: &fasthttp.Server{}}
+	if config.Protocol == "https" {
+		certs := newCerts(config.Cert)
+		m.tf.Get(port).SetHttps(router.Handler(), certs.certs)
 
-			s.srv.Handler = router.Handler()
-			l, err := listener.ListenTcp("", port)
-
-			if err != nil {
-				return err
-			}
-			if config.Protocol == "https" {
-				s.certs = newCerts(config.Cert)
-				s.tlsConfig = &tls.Config{GetCertificate: s.GetCertificate}
-				l = tls.NewListener(l, s.tlsConfig)
-			}
-			go s.srv.Serve(l)
-
-			m.servers[port] = s
-			m.listeners[port] = l
-		}
+	} else {
+		m.tf.Get(port).SetHttp(router.Handler())
 	}
+
+	//
+	//if isCreate {
+	//	s, has := m.servers[port]
+	//	if !has {
+	//
+	//		l, err := m.traffic.ListenTcp("", port)
+	//
+	//		if err != nil {
+	//			return err
+	//		}
+	//		if config.Protocol == "https" {
+	//			s.certs = newCerts(config.Cert)
+	//			s.tlsConfig = &tls.Config{GetCertificate: s.GetCertificate}
+	//			l = tls.NewListener(l, s.tlsConfig)
+	//		}
+	//		go s.srv.Serve(l)
+	//
+	//		m.servers[port] = s
+	//
+	//	}
+	//}
 	return nil
 }
 
@@ -137,15 +110,7 @@ func (m *Manager) Del(port int, id string) error {
 	if r, has := m.routers.Del(port, id); has {
 		//若目标端口的http服务器已无路由配置，则关闭服务器及listener
 		if r.Count() == 0 {
-			if s, has := m.servers[port]; has {
-				err := s.srv.Shutdown()
-				if err != nil {
-					return err
-				}
-				delete(m.servers, port)
-				m.listeners[port].Close()
-				delete(m.listeners, port)
-			}
+			m.tf.ShutDown(port)
 		}
 	}
 
