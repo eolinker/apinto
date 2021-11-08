9bf8d3890145bf391783f167402064189e1b576b
黄孟柱 <hmzzrcs@163.com>
2021-11-08 15:32:18 +0800

    提交

--- filter/next.go
+++ filter/next.go
@@ -3,7 +3,7 @@ package filter
 import "github.com/eolinker/eosc/http"
 
 type NextFilter struct {
-	next IChain
+	next http.IChain
 }
 
 func (n *NextFilter) DoFilter(ctx http.IHttpContext, next http.IChain) error {
