3a204fd2ec9e33fe055475ff53f90818ade94996
黄孟柱 <hmzzrcs@163.com>
2021-11-11 16:30:51 +0800

    增加rewrite插件

--- drivers/plugins/rewrite/driver.go
+++ drivers/plugins/rewrite/driver.go
@@ -1,11 +1,9 @@
-package auth
+package rewrite
 
 import (
-	"fmt"
 	"reflect"
 
 	"github.com/eolinker/eosc"
-	"github.com/eolinker/goku/auth"
 )
 
 type Driver struct {
@@ -35,36 +33,18 @@ func (d *Driver) ConfigType() reflect.Type {
 	return d.configType
 }
 
-func (d *Driver) getList(auths []eosc.RequireId) ([]auth.IAuth, error) {
-	ls := make([]auth.IAuth, 0, len(auths))
-	for _, id := range auths {
-		worker, has := d.workers.Get(string(id))
-		if !has {
-			return nil, fmt.Errorf("%s:%w", id, eosc.ErrorWorkerNotExits)
-		}
-		if !worker.CheckSkill(auth.AuthSkill) {
-			return nil, fmt.Errorf("%s:%w:%s", id, eosc.ErrorTargetNotImplementSkill, auth.AuthSkill)
-		}
-		ls = append(ls, worker.(auth.IAuth))
-
-	}
-	return ls, nil
-}
 func (d *Driver) Create(id, name string, v interface{}, workers map[eosc.RequireId]interface{}) (eosc.IWorker, error) {
 	conf, err := d.check(v)
 	if err != nil {
 		return nil, err
 	}
-	list, err := d.getList(conf.Auth)
-	if err != nil {
-		return nil, err
-	}
-	au := &Auth{
+
+	rw := &Rewrite{
 		Driver: d,
 		id:     id,
 		name:   name,
-		auths:  list,
+		url:    conf.ReWriteUrl,
 	}
 
-	return au, nil
+	return rw, nil
 }
