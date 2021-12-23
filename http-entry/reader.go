378b9c2e567c05cc17cbdf1d457538a377bebcb9
chenjiekun <chenjiekun@sina.cn>
2021-12-23 17:09:44 +0800

    fix reader get Host

--- http-entry/reader.go
+++ http-entry/reader.go
@@ -131,7 +131,7 @@ var (
 			return ctx.Request().Header().GetHeader(strings.Replace(name, "_", "-", -1)), true
 		}),
 		"host": ReadFunc(func(name string, ctx http_service.IHttpContext) (string, bool) {
-			return fmt.Sprintf("%d", time.Now().Unix()), true
+			return ctx.Request().URI().Host(), true
 		}),
 		"error": ReadFunc(func(name string, ctx http_service.IHttpContext) (string, bool) {
 			//TODO 暂时忽略
