501e8a6a223a94d15e3109d175a3358ea0f26740
chenjiekun <chenjiekun@sina.cn>
2022-01-27 19:42:10 +0800

    修改nsq并且测试完成

--- drivers/output/nsq/pool.go
+++ drivers/output/nsq/pool.go
@@ -71,18 +71,11 @@ func (p *producerPool) PublishAsync(topic string, body []byte) error {
 	go func(n int) {
 
 		for attempt := 0; attempt < p.size; attempt++ {
-
-			isLastAttempt := attempt+1 == p.size
 			//轮询
 			index := (n + attempt - 1) % p.size
 			producerNode := p.nodes[index]
-
 			//若该节点不可用
 			if producerNode.status == disconnected {
-				if isLastAttempt {
-					log.Errorf("nsq log error:%s data:%s", errProducerInvalid, fmt.Sprintf("nsqd_addr:%s topic:%s data:%s", producerNode.producer.String(), topic, body))
-					break
-				}
 				continue
 			}
 
@@ -90,16 +83,12 @@ func (p *producerPool) PublishAsync(topic string, body []byte) error {
 			if err := producerNode.producer.Publish(topic, body); err != nil {
 				//发送失败，将该节点状态置为disconnected，等待check重新连接
 				producerNode.status = disconnected
-				if isLastAttempt {
-					log.Errorf("nsq log error:%s data:%s", err, fmt.Sprintf("nsqd_addr:%s topic:%s data:%s", producerNode.producer.String(), topic, body))
-					break
-				}
-
+				log.Errorf("log output nsqd is invalid. nsqd_addr:%s  error:%s", producerNode.producer.String(), err)
 				continue
 			}
-
 			break
 		}
+		log.Errorf("no available nsqd node. data: %s",fmt.Sprintf("topic:%s data:%s", topic, body))
 	}(n)
 
 	return nil
@@ -109,7 +98,7 @@ func (p *producerPool) PublishAsync(topic string, body []byte) error {
 func (p *producerPool) Check() {
 
 	ticker := time.NewTicker(time.Second * 30)
-
+	defer ticker.Stop()
 	ctx, cancelFunc :=context.WithCancel(context.Background())
 	p.cancelFunc = cancelFunc
 	for{
@@ -117,22 +106,34 @@ func (p *producerPool) Check() {
 		select {
 		case <- ticker.C:
 			for _, n := range p.nodes {
-				if n.status == disconnected {
+
+				if err := n.producer.Ping(); err != nil {
 					//解决断线重连的问题
-					n.producer.Stop()
-					if err := n.producer.Ping();err != nil{
+					//n.producer.Stop()
+					//if err = n.producer.Ping();err != nil{
+					//	continue
+					//}
+					//n.status = connecting
+					//continue
+
+					oldProducer := n.producer
+					newProducer,_ := nsq.NewProducer(oldProducer.String(), p.config)
+					if err = newProducer.Ping(); err != nil {
+						if n.status == connecting {
+							n.status = disconnected
+							log.Errorf("log output nsqd is invalid. nsqd_addr:%s  error:%s", oldProducer.String(), err)
+						}
+						newProducer.Stop()
 						continue
 					}
+					n.producer = newProducer
 					n.status = connecting
-
-					//oldProducer := n.producer
-					//n.producer, _ = nsq.NewProducer(oldProducer.String(), p.config)
-					//n.status = connecting
-					//oldProducer.Stop()
+					oldProducer.Stop()
 				}
+				n.status = connecting
+
 			}
 		case <- ctx.Done():
-			ticker.Stop()
 			return
 		}
 
