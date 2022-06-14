dd1186c2021361ba346e9987bf843a4d8dfbd0be
chenjiekun <chenjiekun@sina.cn>
2022-06-14 16:54:19 +0800

    提交

--- drivers/output/nsq/config.go
+++ drivers/output/nsq/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	Topic      string                 `json:"topic" yaml:"topic" label:"topic"`
 	Address    []string               `json:"address" yaml:"address" label:"请求地址"`
 	AuthSecret string                 `json:"auth_secret" yaml:"auth_secret" label:"鉴权secret"`
-	ClientConf map[string]interface{} `json:"-" yaml:"nsq_conf"`
+	ClientConf map[string]interface{} `json:"nsq_conf" yaml:"nsq_conf" skip:""`
 	Type       string                 `json:"type" yaml:"type" enum:"json,line" label:"输出格式"`
 	Formatter  eosc.FormatterConfig   `json:"formatter" yaml:"formatter" label:"格式化配置"`
 }
