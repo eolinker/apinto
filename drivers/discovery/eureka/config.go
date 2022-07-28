f3e6675009bf2ad08c528899b591b6f1af44af00
chenjiekun <chenjiekun@sina.cn>
2022-07-28 17:20:41 +0800

    eureka忽略scheme

--- drivers/discovery/eureka/config.go
+++ drivers/discovery/eureka/config.go
@@ -6,9 +6,11 @@ import (
 	"strings"
 )
 
+const defaultScheme = "http"
+
 //Config eureka驱动配置
 type Config struct {
-	Scheme string       `json:"scheme" label:"请求协议" enum:"HTTP,HTTPS"`
+	Scheme string       `json:"scheme" label:"请求协议" enum:"HTTP,HTTPS" skip:""`
 	Config AccessConfig `json:"config" label:"配置信息"`
 }
 
@@ -18,20 +20,11 @@ type AccessConfig struct {
 	Params  map[string]string `json:"params" label:"参数"`
 }
 
-func (c *Config) getScheme() string {
-	scheme := strings.ToLower(c.Scheme)
-	if scheme != "http" && scheme != "https" {
-		scheme = "http"
-	}
-	return scheme
-}
-
 func (c *Config) getAddress() []string {
-	scheme := c.getScheme()
 	adds := make([]string, 0, len(c.Config.Address))
 	for _, a := range c.Config.Address {
 		if !strings.HasPrefix(a, "http://") && !strings.HasPrefix(a, "https://") {
-			a = fmt.Sprintf("%s://%s", scheme, a)
+			a = fmt.Sprintf("%s://%s", defaultScheme, a)
 		}
 		adds = append(adds, a)
 	}
