ef04ef18b344d82b4b9cee0de03943a43252f9c8
黄孟柱 <hmzzrcs@163.com>
2022-08-15 18:17:03 +0800

    driver

---
+++ v2/router-http-driver/register.go
@@ -0,0 +1,38 @@
+package http_router
+
+import (
+	_ "github.com/eolinker/apinto/router/router-http"
+	"github.com/eolinker/eosc"
+	"github.com/eolinker/eosc/utils/schema"
+	"reflect"
+)
+
+var name = "http_router"
+
+//Register 注册http路由驱动工厂
+func Register(register eosc.IExtenderDriverRegister) {
+	register.RegisterExtenderDriver(name, NewRouterDriverFactory())
+}
+
+//RouterDriverFactory http路由驱动工厂结构体
+type RouterDriverFactory struct {
+}
+
+func (r *RouterDriverFactory) Render() interface{} {
+	render, err := schema.Generate(reflect.TypeOf((*Config)(nil)), nil)
+	if err != nil {
+		return nil
+	}
+	return render
+}
+
+//Create 创建http路由驱动
+func (r *RouterDriverFactory) Create(profession string, name string, label string, desc string, params map[string]interface{}) (eosc.IExtenderDriver, error) {
+ 
+	return NewHTTPRouterDriver(), nil
+}
+
+//NewRouterDriverFactory 创建一个http路由驱动工厂
+func NewRouterDriverFactory() *RouterDriverFactory {
+	return &RouterDriverFactory{}
+}
---
+++ v2/router-http-manager/append.go
@@ -0,0 +1,5 @@
+package router_http_manager
+
+import "github.com/eolinker/apinto/v2/router"
+
+type AppendRule = router.AppendRule
