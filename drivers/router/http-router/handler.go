bea0c635ddbf344a582c1c32dce68e4876dc3c0a
黄孟柱 <hmzzrcs@163.com>
2022-08-16 17:10:23 +0800

    生效禁用

--- drivers/router/http-router/handler.go
+++ drivers/router/http-router/handler.go
@@ -3,6 +3,8 @@ package http_router
 import (
 	"github.com/eolinker/apinto/service"
 	"github.com/eolinker/eosc/eocontext"
+	http_context "github.com/eolinker/eosc/eocontext/http-context"
+	"net/http"
 )
 
 type Handler struct {
@@ -10,9 +12,20 @@ type Handler struct {
 	finisher        Finisher
 	service         service.IService
 	filters         eocontext.IChain
+	disable         bool
 }
 
 func (h *Handler) ServeHTTP(ctx eocontext.EoContext) {
+	if h.disable {
+		httpContext, err := http_context.Assert(ctx)
+		if err != nil {
+			return
+		}
+		httpContext.Response().SetStatus(http.StatusNotFound, "")
+		httpContext.Response().SetBody([]byte("router disable"))
+		httpContext.FastFinish()
+		return
+	}
 	ctx.SetFinish(&h.finisher)
 	ctx.SetCompleteHandler(&h.completeHandler)
 	ctx.SetApp(h.service)
