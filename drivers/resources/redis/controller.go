c7415928e2e2323bfe55f29367c2ef5e291d16dc
黄孟柱 <hmzzrcs@163.com>
2022-10-12 15:07:01 +0800

    缓存

--- drivers/resources/redis/controller.go
+++ drivers/resources/redis/controller.go
@@ -1,8 +1,11 @@
 package redis
 
 import (
+	"context"
+	"github.com/eolinker/apinto/resources"
 	"github.com/eolinker/eosc"
 	"github.com/eolinker/eosc/env"
+	"github.com/eolinker/eosc/log"
 	"github.com/go-redis/redis/v8"
 	"reflect"
 )
@@ -15,17 +18,63 @@ type Controller struct {
 func (m *Controller) ConfigType() reflect.Type {
 	return configType
 }
-
+func (m *Controller) shutdown() {
+	oldClient := m.current
+	if oldClient != nil {
+		m.current = nil
+		resources.Replace()
+		oldClient.client.Close()
+	}
+}
 func (m *Controller) Set(conf interface{}) (err error) {
 	config, ok := conf.(*Config)
 	if ok && config != nil {
 		old := m.config
 		m.config = *config
 
-		if env.Process() == eosc.ProcessWorker {
-			// todo open or close redis
+		if reflect.DeepEqual(old, m.config) {
+			return nil
 		}
-		redis.NewClusterClient().Close()
+
+		if len(m.config.Addrs) == 0 {
+			oldClient := m.current
+			if oldClient != nil {
+				resources.Replace()
+				m.current = nil
+				oldClient.client.Close()
+			}
+			return nil
+		}
+
+		client := redis.NewClusterClient(&redis.ClusterOptions{
+			Addrs:    m.config.Addrs,
+			Username: m.config.Username,
+			Password: m.config.Password,
+		})
+
+		if res, errPing := client.Ping(context.Background()).Result(); errPing != nil {
+			log.Info("ping redis:", res, " error:", err)
+			client.Close()
+			return errPing
+		}
+
+		if env.Process() == eosc.ProcessWorker {
+			if m.current == nil {
+				m.current = newCacher(client)
+				resources.Replace(m.current)
+			} else {
+				m.current.client = client
+			}
+		} else {
+			client.Close()
+		}
+	} else {
+		oldClient := m.current
+		if oldClient != nil {
+			resources.Replace()
+			m.current = nil
+			oldClient.client.Close()
+		}
 	}
 	return nil
 }
