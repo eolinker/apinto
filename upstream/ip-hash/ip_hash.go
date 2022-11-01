aa7d174e97d94429690b4f0f1deeb65b880047d6
yangdingshan <yangdsh1@sugon.com>
2022-11-01 18:01:26 +0800

    修改ip hash负载均衡算法

--- upstream/ip-hash/ip_hash.go
+++ upstream/ip-hash/ip_hash.go
@@ -1,21 +1,15 @@
 package ip_hash
 
 import (
-	"errors"
 	"github.com/eolinker/apinto/discovery"
 	"github.com/eolinker/apinto/upstream/balance"
 	eoscContext "github.com/eolinker/eosc/eocontext"
 	http_service "github.com/eolinker/eosc/eocontext/http-context"
 	"hash/crc32"
-	"time"
 )
 
 const (
 	name = "ip-hash"
-)
-
-var (
-	errNoReadIp = errors.New("read ip is null")
 )
 
 // Register 注册ip-hash算法
@@ -36,29 +30,8 @@ func (r *ipHashFactory) Create(app discovery.IApp) (eoscContext.BalanceHandler, 
 	return rr, nil
 }
 
-type node struct {
-	weight int
-	discovery.INode
-}
-
 type ipHash struct {
 	app discovery.IApp
-	// nodes 节点列表
-	nodes []node
-	// 节点数量
-	size int
-	// index 当前索引
-	index int
-	// gcdWeight 权重最大公约数
-	gcdWeight int
-	// maxWeight 权重最大值
-	maxWeight int
-
-	cw int
-
-	updateTime time.Time
-
-	downNodes map[int]discovery.INode
 }
 
 func (r *ipHash) Select(ctx eoscContext.EoContext) (eoscContext.INode, error) {
@@ -67,42 +40,20 @@ func (r *ipHash) Select(ctx eoscContext.EoContext) (eoscContext.INode, error) {
 
 // Next 由现有节点根据ip_hash决策出一个可用节点
 func (r *ipHash) Next(org eoscContext.EoContext) (discovery.INode, error) {
-	if time.Now().Sub(r.updateTime) > time.Second*30 {
-		// 当上次节点更新时间与当前时间间隔超过30s，则重新设置节点
-		r.set()
-	}
-	ctx, err := http_service.Assert(org)
+	httpContext, err := http_service.Assert(org)
 	if err != nil {
 		return nil, err
 	}
-	readIp := ctx.Request().ReadIP()
-	if len(readIp) == 0 {
-		return nil, errNoReadIp
-	}
-	ipHash := HashCode(readIp)
-	index := ipHash % r.size
-	return r.nodes[index], nil
+	readIp := httpContext.Request().ReadIP()
+	nodes := r.app.Nodes()
+	return nodes[HashCode(readIp)%len(nodes)], nil
 }
 
 func newIpHash(app discovery.IApp) *ipHash {
 	r := &ipHash{
 		app: app,
 	}
-	r.set()
 	return r
-}
-
-func (r *ipHash) set() {
-	r.downNodes = make(map[int]discovery.INode)
-	nodes := r.app.Nodes()
-	r.size = len(nodes)
-	ns := make([]node, 0, r.size)
-	for _, n := range nodes {
-		nd := node{1, n}
-		ns = append(ns, nd)
-	}
-	r.nodes = ns
-	r.updateTime = time.Now()
 }
 
 func HashCode(s string) int {
