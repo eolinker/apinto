a12d432b1f26d689a0c09ae90e27d89200587f65
Liujian <824010343@qq.com>
2022-12-14 11:28:10 +0800

    新增自动打包上传到docker内部镜像脚本

---
+++ build/cmd/docker-build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+IS_ARM=$1
+
+IS_LATEST=$2
+
+ORG_PATH=$(pwd)
+
+COMMIT_ID=$(git rev-parse --short HEAD)
+VERSION=$(git tag --contain ${COMMIT_ID} | awk 'END {print}')
+
+if [ "${VERSION}" == "" ];then
+	echo "[ERROR] Without tag commit."
+	exit 1
+fi
+
+cd ../../ && GOVERSION=$(go version) EoscVersion=$(sed -n 's/.*eosc v/v/p' go.mod) goreleaser release --skip-publish --rm-dist
+
+cd $(pwd)
+
+cp ../../dist/apinto_${VERSION}_linux_amd64.tar.gz apinto.linux.x64.tar.gz
+
+PLATFORM=""
+if [ $IS_ARM == "true" ];then
+	PLATFORM="--platform=linux/amd64"
+fi
+
+docker build ${PLATFORM} -t "docker.eolinker.com/docker/apinto:${VERSION}" ./
+
+docker push "docker.eolinker.com/docker/apinto:${VERSION}"
+
+if [ $IS_LATEST == "true" ];then
+	docker tag "docker.eolinker.com/docker/apinto:${VERSION}" "docker.eolinker.com/docker/apinto:latest"
+	docker push "docker.eolinker.com/docker/apinto:latest"
+fi
+
+rm apinto.linux.x64.tar.gz
