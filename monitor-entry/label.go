eda73a154b7c9a17b48c107ba358d103f06e609b
Liujian <824010343@qq.com>
2023-01-11 15:01:01 +0800

    influxdb批量完成

--- monitor-entry/label.go
+++ monitor-entry/label.go
@@ -1,17 +1,13 @@
 package monitor_entry
 
 var (
-	LabelNode     = "node"
-	LabelCluster  = "cluster"
 	LabelApi      = "api"
 	LabelApp      = "app"
-	LabelHandler  = "handler"
 	LabelUpstream = "upstream"
 )
 
 var labels = map[string]string{
 	LabelApi:      "api_id",
 	LabelApp:      "application_id",
-	LabelHandler:  "handler",
 	LabelUpstream: "service_id",
 }
