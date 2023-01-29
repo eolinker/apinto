f8da127fcf36c7cc9b8d842fe7023bba4e15a3d6
Liujian <824010343@qq.com>
2023-01-29 16:43:31 +0800

    证书驱动修改label和description

--- drivers/certs/config.go
+++ drivers/certs/config.go
@@ -2,6 +2,6 @@ package certs
 
 type Config struct {
 	Name string `json:"name" label:"证书名"`
-	Key  string `json:"key" label:"key"`
-	Pem  string `json:"pem" label:"value"`
+	Key  string `json:"key" label:"密钥内容" format:"text" description:"密钥文件的后缀名一般为.key"`
+	Pem  string `json:"pem" label:"证书内容" format:"text" description:"证书文件的后缀名一般为.crt 或 .pem"`
 }
