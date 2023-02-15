3833144ba2ae2841aaf3c35995e5e2fe107aa263
zhangzeyi <zhangzeyi@eolink.com>
2023-02-15 18:49:07 +0800

    删除httpservice

--- drivers/plugins/dubbo2-proxy-rewrite/proxy-rewrite.go
+++ drivers/plugins/dubbo2-proxy-rewrite/proxy-rewrite.go
@@ -5,16 +5,10 @@ import (
 	"github.com/eolinker/eosc"
 	"github.com/eolinker/eosc/eocontext"
 	dubbo2_context "github.com/eolinker/eosc/eocontext/dubbo2-context"
-	http_service "github.com/eolinker/eosc/eocontext/http-context"
 )
 
 var _ eocontext.IFilter = (*ProxyRewrite)(nil)
 var _ dubbo2_context.DubboFilter = (*ProxyRewrite)(nil)
-
-var (
-	regexpErrInfo   = `[plugin proxy-rewrite2 config err] Compile regexp fail. err regexp: %s `
-	notMatchErrInfo = `[plugin proxy-rewrite2 err] Proxy path rewrite fail. Request path can't match any rewrite-path. request path: %s `
-)
 
 type ProxyRewrite struct {
 	drivers.WorkerBase
@@ -70,5 +64,5 @@ func (p *ProxyRewrite) Destroy() {
 }
 
 func (p *ProxyRewrite) CheckSkill(skill string) bool {
-	return http_service.FilterSkillName == skill
+	return dubbo2_context.FilterSkillName == skill
 }
