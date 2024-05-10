afbf005c45a9e6e9cfb96014d57a270ed27b71b3
Liujian <824010343@qq.com>
2024-05-10 17:03:37 +0800

    修复轮询算法高并发报no valid问题

--- upstream/round-robin/round_robin_test.go
+++ upstream/round-robin/round_robin_test.go
@@ -1,12 +1,13 @@
 package round_robin
 
 import (
-	"github.com/eolinker/apinto/drivers/discovery/static"
-	"github.com/eolinker/eosc/eocontext"
 	"runtime"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/eolinker/apinto/drivers/discovery/static"
+	"github.com/eolinker/eosc/eocontext"
 )
 
 type demoNode struct {
@@ -106,4 +107,48 @@ func Test_roundRobin_Next_Retry_Status(t *testing.T) {
 
 	}
 	wg.Wait()
+}
+
+func Test_roundRobin_Next_Retry_Status_2(t *testing.T) {
+	discovery := static.CreateAnonymous(&static.Config{
+		HealthOn: false,
+		Health:   nil,
+	})
+
+	app, err := discovery.GetApp("")
+	if err != nil {
+		return
+	}
+	robin := newRoundRobin(app, "http", time.Second)
+	for i := 0; i < 12; i++ {
+		n, _, err := robin.Select(nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Log(i, n.Addr())
+	}
+}
+
+var ()
+
+func Benchmark_roundRobin_Next_Retry_demo(b *testing.B) {
+	discovery := static.CreateAnonymous(&static.Config{
+		HealthOn: false,
+		Health:   nil,
+	})
+	app, err := discovery.GetApp("127.0.0.1:8080;127.0.0.1:8081;127.0.0.1:8083;127.0.0.1:8084")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	robin := newRoundRobin(app, "http", time.Second)
+	for i := 0; i < b.N; i++ {
+		_, _, err := robin.Select(nil)
+		if err != nil {
+			b.Fatal(err)
+			return
+		}
+		//b.Log(i, n.Addr())
+	}
 }
