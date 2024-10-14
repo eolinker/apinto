05c550f96eef122e03c79f0178899f939ef3b6c3
lidongjie <limingliulise@outlook.com>
2024-10-14 16:13:31 +0800

    add:添加cohere供应商

---
+++ drivers/ai-provider/cohere/message.go
@@ -0,0 +1,37 @@
+package cohere
+
+type ClientRequest struct {
+	Messages []*RequestMessage `json:"messages"`
+}
+
+type RequestMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Response struct {
+	Id           string          `json:"id"`
+	Message      ResponseMessage `json:"message"`
+	Usage        Usage           `json:"usage"`
+	FinishReason string          `json:"finish_reason"`
+}
+
+type ResponseContent struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type ResponseMessage struct {
+	Role    string            `json:"role"`
+	Content []ResponseContent `json:"content"`
+}
+
+type Tokens struct {
+	InputTokens  int `json:"input_tokens"`
+	OutputTokens int `json:"output_tokens"`
+}
+
+type Usage struct {
+	BilledUnits Tokens `json:"billed_units"`
+	Tokens      Tokens `json:"tokens"`
+}
