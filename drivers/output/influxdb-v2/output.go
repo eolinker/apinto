7f5fe46cfe047188261dc6e5d5f1c7acb1defb13
Liujian <824010343@qq.com>
2026-08-26 10:56:36 +0800

    influxdb问题修复

--- drivers/output/influxdb-v2/output.go
+++ drivers/output/influxdb-v2/output.go
@@ -152,7 +152,7 @@ func (o *Output) Output(entry eosc.IEntry) error {
 		}
 	}
 
-	msec := eosc.ReadStringFromEntry(entry, "now_nano")
+	msec := eosc.ReadStringFromEntry(entry, "now")
 	msecInt, _ := strconv.ParseInt(msec, 10, 64)
 	var timestamp time.Time
 	if msecInt > 0 {
